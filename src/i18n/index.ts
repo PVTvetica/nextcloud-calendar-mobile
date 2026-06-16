b429411e36b6ad1e5b6b58917958409e1e3a101d
Charles GTE <charlesgte31@gmail.com>
2026-06-16 09:19:12 +0200

    feat: initialize i18next with locale resources and dayjs locales

---
+++ src/i18n/index.ts
@@ -0,0 +1,24 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import 'dayjs/locale/fr';
+import 'dayjs/locale/de';
+import 'dayjs/locale/es';
+import en from './locales/en.json';
+import fr from './locales/fr.json';
+import de from './locales/de.json';
+import es from './locales/es.json';
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: { translation: en },
+    fr: { translation: fr },
+    de: { translation: de },
+    es: { translation: es },
+  },
+  lng: 'en',
+  fallbackLng: 'en',
+  returnEmptyString: false,
+  interpolation: { escapeValue: false },
+});
+
+export default i18n;
